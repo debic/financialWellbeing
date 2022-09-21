a43c9e5e9757dff380a559b2a8f9a12e979b6e11
Debi Codriansky <debicps@gmail.com>
2022-09-21 13:24:27 +0300

    Basic structure dashboard

---
+++ src/Components/Cards/GeneralScoreCard.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function GeneralScoreCard() {
+  return (
+    <div className='scoreCards_general'>
+
+        <div className='scoreCards_general_info'>
+            <div className='scoreCards_general_title_div'>
+                <h3 className='scoreCards_general_title'>General score</h3>
+                <p className='scoreCards_general_info_users'>100000 Answers</p>
+            </div>
+
+            <h2 className='scoreCards_info'>20<span>/100</span></h2>
+        </div>
+        <div className='scoreCards_general_graphs'>
+
+            <div className='scoreCards_general_graph1'>
+                
+            </div>
+
+            <div className='scoreCards_general_graph2'>
+                
+            </div>
+
+        </div>
+    
+    </div>
+  )
+}
--- src/Components/Cards/Q4Card.js
+++ src/Components/Cards/Q4Card.js
@@ -1,11 +1,10 @@
 import React from 'react'
 
-export default function Q1Card() {
+export default function Q4Card() {
   return (
     <div className='scoreCards'>
         <h3 className='scoreCards_title'> Question 1</h3>
         <h2 className='scoreCards_info'>20<span>/100</span></h2>
         <p className='scoreCards_info_users'>100000 Answers</p>
-    </div>
-  )
+    </div>  )
 }
