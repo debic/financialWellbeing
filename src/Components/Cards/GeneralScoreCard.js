5764041ea2270eda1601680ee5bdb516fd515b1e
Debi Codriansky <debicps@gmail.com>
2022-09-21 18:21:40 +0300

    ChartsÃ

--- src/Components/Cards/GeneralScoreCard.js
+++ src/Components/Cards/GeneralScoreCard.js
@@ -1,6 +1,24 @@
-import React from 'react'
-
+import React, {useState} from 'react'
+import MunicipalityComparation from '../Graphs/MunicipalityComparation'
+import MunicipalityScoreYears from '../Graphs/MunicipalityScoreYears'
+import axios from 'axios'
 export default function GeneralScoreCard() {
+
+const[numb, setNumb] = useState("90") 
+
+    async function getNumb() {
+        try {
+          const res = await axios.get(
+            "http://localhost:8080/answers/count", {params: {question: "Do you save money?"}}
+          );
+          console.log(res)
+        } catch (err) {
+          console.log(err);
+        }
+      }
+
+      getNumb()
+
   return (
     <div className='scoreCards_general'>
 
@@ -10,16 +28,15 @@ export default function GeneralScoreCard() {
                 <p className='scoreCards_general_info_users'>100000 Answers</p>
             </div>
 
-            <h2 className='scoreCards_info'>20<span>/100</span></h2>
+            <h2 className='scoreCards_info'>{numb}<span>/100</span></h2>
         </div>
         <div className='scoreCards_general_graphs'>
 
             <div className='scoreCards_general_graph1'>
-                
             </div>
 
             <div className='scoreCards_general_graph2'>
-                
+                <MunicipalityComparation/>
             </div>
 
         </div>
--- src/Components/Cards/Q2Card.js
+++ src/Components/Cards/Q2Card.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Q2Chart from '../Graphs/Q2Chart';
 
 export default function Q2Card() {
   return (
     <div className='scoreCards'>
-    <h3 className='scoreCards_title'> Question 1</h3>
-    <h2 className='scoreCards_info'>20<span>/100</span></h2>
+    <h3 className='scoreCards_title'> Because of my money situation...I will never have the things I want in life</h3>
+    <Q2Chart/>
     <p className='scoreCards_info_users'>100000 Answers</p>
     </div>
   )
