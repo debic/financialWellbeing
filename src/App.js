eb8c00059708482369e314c5b7a7626b97089be7
Debi Codriansky <debicps@gmail.com>
2022-09-21 10:45:17 +0300

    Routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Dashboard from './Pages/Dashboard';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="">
+      <Routes>
+      <Route path="/" element={ <Dashboard/>}/>
+      </Routes>
       </header>
     </div>
+    </BrowserRouter>
   );
 }
 
